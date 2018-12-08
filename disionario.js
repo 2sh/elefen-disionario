50b3036b04eae3088735c20181e2026f28b85ab1
elefen <x@x.x>
2018-12-08 05:51:07 +0000

    2018-12-08 05:51:04

--- disionario.js
+++ disionario.js
@@ -1,4 +1,4 @@
-﻿var dataDeDatos = '5 desembre 2018';
+﻿var dataDeDatos = '7 desembre 2018';
 var disionario = [
 {T:"r",L:"a",C:["sim"],d:"la letera prima de la alfabeta roman, e sua nom; la nota ses de la scala diatonica de C major",p:"a",en:"a",fr:"la",he:"האות הראשונה באלפבית הלטיני"},
 {T:"r",L:"a",C:["prep"],d:"indicante un loca o tempo como un punto simple",g:"Preposadas#A",u:"nos senta a la table; la barco es a mar; me va parti a la fini de la semana; a 26 julio 1887; a la ora des-sinco",en:"at, in, on (a point in space or time)",eo:"ĉe, en, je",fr:"à",he:"ב-, אצל",ru:"в, на, за (сидим за столом)"},
@@ -7472,7 +7472,7 @@ var disionario = [
 {T:"r",L:"declina",C:["vnt","ling"],d:"diminui en fortia o cualia; (un nom) cambia sua forma gramatical per indica caso, numero e jenero",en:"decline (including grammar)",fr:"décliner (baisser, grammaire)",ru:"склонять (грамматический термин)"},
 {L:"declina",C:["n"],en:"declension, declination; downturn",fr:"déclin; déclinaison",ru:"склонение"},
 {T:"r",L:"deco",C:["a","arte"],d:"pertinente a un stilo de arte e decora de sirca 1920-1930, marcada par formas jeometrial e colores forte, spesial en arciteta",en:"art deco"},
-{T:"r",L:"decora",C:["vt","arci"],d:"beli la aspeta de (un cosa) par ajuntas; dona un premio o medalia a (algun)",en:"decorate (including with an honour), adorn, trim, garnish",fr:"décorer",ru:"декорировать, украшать"},
+{T:"r",L:"decora",C:["vt","arci"],d:"beli la aspeta de (un cosa) par ajuntas; dona un premio o medalia a (algun)",en:"decorate (including with an honor|ur), adorn, trim, garnish",fr:"décorer",ru:"декорировать, украшать"},
 {L:"decora",C:["n"],en:"decoration, decor, ornament, garnish, adornment, trim, topping; scenery (theatrical)",fr:"décoration (incluant un honneur)",ru:"декорация, украшение, орнамент"},
 {L:"decoror",C:["n"],en:"decorator",fr:"décorateur",ru:"декоратор"},
 {L:"nondecorada",C:["a"],en:"undecorated, blank, plain",fr:"sans décor",ru:"без украшений, неукрашенный, простой"},
